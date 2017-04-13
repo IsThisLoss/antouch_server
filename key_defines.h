4cb5b8f20fb998b1296697bb65d461b50cc0a2aa
IsThisLoss <KopturovDim@gmail.com>
2017-04-13 20:39:00 +0300

    It's okay2

---
+++ key_defines.h
@@ -0,0 +1,29 @@
+//
+// Created by isthisloss on 13.04.17.
+//
+
+#ifndef ANTOUCH_SERVER_KEY_DEFINES_H
+#define ANTOUCH_SERVER_KEY_DEFINES_H
+
+// 0 or 1 are half-click events
+#define HOLD_ON 0
+#define HOLD_OFF 1
+
+// 10 < x < 20 are mouse click events
+#define LEFT_CLICK 14
+#define RIGHT_CLICK 15
+
+// 20 < x < 30 are mouse motions events
+#define MOVE 28
+#define V_SCROLL 29
+
+// x > 50 are keys' events
+#define BUTTON_UP 51
+#define BUTTON_DOWN 52
+#define BUTTON_LEFT 54
+#define BUTTON_RIGHT 55
+#define BUTTON_VOL_DOWN 57
+#define BUTTON_VOL_UP 58
+
+
+#endif //ANTOUCH_SERVER_KEY_DEFINES_H
